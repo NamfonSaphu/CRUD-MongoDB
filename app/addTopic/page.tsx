c19819e1e0879c1d0b6c9210d122a57dd0170e93
NamfonSaphu <namfonsaphu@gmail.com>
2025-05-04 21:01:27 +0700

    edit color

--- app/addTopic/page.tsx
+++ app/addTopic/page.tsx
@@ -35,8 +35,8 @@ export default function AddTopic() {
   return (
 
     <form onSubmit={handleSubmit} className="grid w-full w=[750] items-center gap-1.5">
-      <Input onChange={(e) => setTitle(e.target.value)} value={title} type="text" placeholder="Topic Title" className="bg-violet-200/10 placeholder-zinc-700" />
-      <Input onChange={(e) => setDescription(e.target.value)} value={description} type="text" placeholder="Topic Description" className="bg-violet-200/10 placeholder-zinc-700" />
+      <Input onChange={(e) => setTitle(e.target.value)} value={title} type="text" placeholder="Topic Title" className="bg-violet-200/10 placeholder-zinc-700 text-white" />
+      <Input onChange={(e) => setDescription(e.target.value)} value={description} type="text" placeholder="Topic Description" className="bg-violet-200/10 placeholder-zinc-700 text-white" />
       <div>
         <button type="submit" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-md text-sm px-5 py-2.25 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Add Topic</button>
       </div>
